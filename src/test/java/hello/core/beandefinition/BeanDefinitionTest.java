fdbbc8f4a29e7b348c43e4356b7a7d953d3f8d60
baeyuyeon <dbdus98@gmail.com>
2023-03-23 10:03:01 +0900

    스프링 컨테이너와 스프링 빈

--- src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
+++ src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
@@ -1,32 +1,26 @@
-package hello.core.beanfind;
+package hello.core.beandefinition;
 
 import hello.core.AppConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
-class ApplicationContextInfoTest {
+public class BeanDefinitionTest {
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    //GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+
     @Test
-    @DisplayName("모든 빈 출력하기")
-    void findAllBean(){
-        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
-        for (String beanDefinitionName : beanDefinitionNames) {
-            Object bean = ac.getBean(beanDefinitionName);
-            System.out.println("name = " + beanDefinitionName+ " object = "+bean);
-        }
-    }
-    @Test
-    @DisplayName("애플리케이션 빈 출력하기")
+    @DisplayName("빈 설정 메타정보 확인")
     void findApplicationBean(){
         String[] beanDefinitionNames = ac.getBeanDefinitionNames();
         for (String beanDefinitionName : beanDefinitionNames) {
             BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
-
             if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION){
-                Object bean = ac.getBean(beanDefinitionName);
-                System.out.println("name = " + beanDefinitionName+ " object = "+bean);
+                System.out.println("beanDefinitionName = " + beanDefinitionName +
+                        "  beanDefinition = "+ beanDefinition);
             }
         }
     }
